4457b3a381cefa44f1a1ad6bd58d932a7368dafb
premrasapalli <premrasapalli82@gmail.com>
2025-11-25 15:57:22 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.21.0"
@@ -15,19 +5,19 @@ module "eks" {
   cluster_name    = "goway-cluster"
   cluster_version = "1.29"
 
-  vpc_id      = "vpc-02a73c94354f271c8"
+  vpc_id = "vpc-02a73c94354f271c8"
 
-  subnet_ids  = [
+  subnet_ids = [
     "subnet-0fdcd74e653ed3971",
     "subnet-0a41b261e1bd12593"
   ]
 
-  node_groups = {
+  eks_managed_node_groups = {
     goway_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_types   = ["t3.medium"]
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+      instance_types = ["t3.medium"]
     }
   }
 }
